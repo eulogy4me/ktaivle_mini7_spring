a6b371b9bd6a87c354e6871968d4c23b8474c712
sunchae <tjsco94@naver.com>
2024-12-23 15:28:18 +0900

    작업내용 저장

--- src/main/java/com/aivle/mini7/client/api/FastApiClient.java
+++ src/main/java/com/aivle/mini7/client/api/FastApiClient.java
@@ -1,21 +1,29 @@
 package com.aivle.mini7.client.api;
 
 
-import com.aivle.mini7.client.dto.HospitalResponse;
+import com.aivle.mini7.client.dto.FastApiResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
 /**
  * FastApiClient
- * @app.get("/hospital/{request}/{latitude}/{longitude}") 를 호출한다.
+ * FastAPI의 "/recommend_hospital/{filename}") 엔드포인트를 호출한다.
  */
 @FeignClient(name = "fastApiClient", url = "${hospital.api.host}")
 public interface FastApiClient {
 
-     @GetMapping("/hospital_by_module")
-     public List<HospitalResponse> getHospital(@RequestParam("request") String request, @RequestParam("latitude") double latitude, @RequestParam("longitude") double longitude);
 
+     @GetMapping("/fastapi_hospital/{filename}")
+     FastApiResponse throughPipeLine(@PathVariable("filename") String filename);
+
+
+     @GetMapping("/test") // FastAPI의 엔드포인트와 매핑
+     String testFastApi(); // JSON 응답을 String으로 받아 확인
 }
+
+
